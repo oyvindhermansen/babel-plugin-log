48903c5ee5318c418d4fe6abc6db682110c66847
oyvindhermansen <oyvind@ludensreklame.no>
2017-10-16 00:38:25 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+export default function({ types: t }) {
+  return {
+    visitor: {
+      Identifier(path) {
+        const isLogStatement = path.node.name === 'log';
+
+        if (!isLogStatement) return;
+
+        const prefix = `console.`;
+        path.node.name = `${t.stringLiteral(prefix).value}${path.node.name}`;
+      }
+    }
+  };
+}
